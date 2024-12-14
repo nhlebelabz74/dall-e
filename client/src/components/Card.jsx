55d3501ff07aefe97746e7a542dace1ddf16356c
Banzile <banzile.nhlebela74@gmail.com>
2024-12-14 16:16:56 +0200

    cors fix

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -24,7 +24,7 @@ const Card = ({ id, name, prompt, image }) => {
                     </div>
 
                     <button
-                        onClick={() => downloadImage({ id: id, image: image })}
+                        onClick={() => downloadImage({ id: id, url: image })}
                         className="flex gap-2 items-center rounded-full bg-blue"
                     >
                         <img
