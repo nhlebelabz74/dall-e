bf753291a077ddb8fcae5631042d7b0c2c406e26
Banzile <banzile.nhlebela74@gmail.com>
2024-12-14 03:54:10 +0200

    added cors stuff to try fix downloading functionality

--- server/app.js
+++ server/app.js
@@ -20,6 +20,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/v1', [postRouter, dalleRouter]);
 app.use(errorHandler);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.get('/home', (req, res) => {
     res.status(200).send('Hello from the DALL-E API');
